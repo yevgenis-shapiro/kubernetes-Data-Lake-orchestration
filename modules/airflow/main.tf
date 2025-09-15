784bf930ce53e370871af158a7e61a0e623f75da
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-09-16 01:20:16 +0300

    update

--- modules/airflow/main.tf
+++ modules/airflow/main.tf
@@ -7,8 +7,9 @@ resource "helm_release" "airflow" {
   version    = "1.15.0" # pick a stable chart version
   create_namespace = true
 
-  atomic           = true
+  atomic          = false   # prevents automatic rollback on timeout
   cleanup_on_fail  = true
+  timeout         = 900     # 15 min, Airflow may take long to deploy
 
   values = [
     file("${path.module}/airflow-values.yaml")
